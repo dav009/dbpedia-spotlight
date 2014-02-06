c25dd1ccce8bbf44536be2511952a0e641e7c2f4
David Przybilla <dav.alejandro@gmail.com>
2014-02-06 14:41:30 +0000

    outputing relevance scores

--- core/src/main/scala/org/dbpedia/spotlight/relevance/RelevanceDistanceToTextContext.scala
+++ core/src/main/scala/org/dbpedia/spotlight/relevance/RelevanceDistanceToTextContext.scala
@@ -119,7 +119,7 @@ class RelevanceDistanceToTextContext(val contextStore:ContextStore)  extends Rel
     val firstScore = mutable.HashMap[DBpediaResource, Double]()
 
     topicVectors.keys foreach { dbpediaTopic: DBpediaResource =>
-      if (numberOfTokensInCommon(dbpediaTopic)>0){
+      if (numberOfTokensInCommon.getOrElse(dbpediaTopic,0)>0){
         firstScore(dbpediaTopic) = scores(dbpediaTopic)
         scores(dbpediaTopic) = scores(dbpediaTopic) / numberOfTokensInCommon(dbpediaTopic)
       }
